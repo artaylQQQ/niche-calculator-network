31e61813f6fefada00ec70d506d1b07a5969fde4
artaylQQQ <arturoaylwinr@gmail.com>
2025-08-19 00:34:25 -0400

    Calculadoras_V024_2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+if (!process.env.SITE_URL) { throw new Error('SITE_URL is required'); }
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
