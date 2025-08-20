23613db15f74a71fba65b491fd4ca4f83dd302cf
artaylQQQ <arturoaylwinr@gmail.com>
2025-08-20 04:30:25 -0400

    Calculadoras_V025_2 estable pero fea

---
+++ Calculadoras_V025_2/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+if (!process.env.SITE_URL) { throw new Error('SITE_URL is required'); }
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+const SITE_URL = process.env.SITE_URL || 'https://example.com';
+
+export default defineConfig({
+  integrations: [tailwind({ config: { applyBaseStyles: true } })],
+  site: SITE_URL,
+  integrations: [mdx(), sitemap()],
+});
