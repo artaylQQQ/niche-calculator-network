353b303c3e688f30ff1adada05584b159a0f2ebf
artaylQQQ <arturoaylwinr@gmail.com>
2025-08-24 00:29:15 -0400

    Delete src directory

--- src/utils/format.js
+++
@@ -1,7 +0,0 @@
-export function formatResult(value, isMoney = false, currency = "$") {
-  if (typeof value === "number") {
-    let v = Number(value.toFixed(3));
-    return isMoney ? `${currency}${v}` : v;
-  }
-  return value;
-}
