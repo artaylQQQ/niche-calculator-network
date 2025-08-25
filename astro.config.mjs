fcf62429ad2aac4be2f285078ff4f01b6259a9a3
artaylQQQ <arturoaylwinr@gmail.com>
2025-08-25 01:30:39 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-if (!process.env.SITE_URL) { throw new Error('SITE_URL is required'); }
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-const SITE_URL = process.env.SITE_URL || 'https://calcsimpler.com';
+// Usa el valor de SITE_URL si existe, o un valor por defecto.
+const SITE_URL = process.env.SITE_URL || 'https://example.com';
 
 export default defineConfig({
-  // unify all integrations into a single array. Tailwind should run alongside mdx and sitemap.
   site: SITE_URL,
   integrations: [
     tailwind({ config: { applyBaseStyles: true } }),
