cb89aa865a904003e6311e29aee284b4b868687f
artaylQQQ <arturoaylwinr@gmail.com>
2025-09-03 16:10:51 -0400

    feat: apply Equilibrio Moderno color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
-  theme: { extend: { container: { center: true, padding: "1rem" } } },
+  theme: {
+    extend: {
+      container: { center: true, padding: "1rem" },
+      colors: {
+        base: {
+          black: "#000000",
+          gray: "#6b7280",
+          white: "#ffffff"
+        },
+        accent: {
+          red: "#ef4444",
+          blue: "#2563eb",
+          yellow: "#facc15"
+        }
+      }
+    }
+  },
   plugins: []
 };
